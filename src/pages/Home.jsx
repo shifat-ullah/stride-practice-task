e822d7bf64ab650bb79218a8bd7de293c7de4cb8
unknown <shifatullah128@gmail.com>
2024-05-26 11:30:27 +0600

    category and card data rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,27 +8,15 @@ export default function Home() {
   const [recipes, setRescipes] = useState();
   const [categoris, setCategories] = useState();
   useEffect(() => {
-    // fetch("http://localhost:3000/recipes")
-    //   .then((res) => res.json())
-    //   .then((data) => setRescipes(data));
+    fetch("http://localhost:3000/recipes")
+      .then((res) => res.json())
+      .then((data) => setRescipes(data));
 
-    async function load() {
-      //get recipies
-      const recipeRes = await fetch("http://localhost:3000/recipes");
-      const recipeData = await recipeRes.json();
-      setRescipes(recipeData);
-      //get categories
+    
 
-      const categoryRes = await fetch("http://localhost:3000/categories");
-      const categoryData = await categoryRes.json();
-
-      setCategories(categoryData);
-    }
-    load();
-
-    // fetch("http://localhost:3000/categories")
-    //   .then((res) => res.json())
-    //   .then((data) => setCategories(data));
+    fetch("http://localhost:3000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
   }, []);
 
   console.log("hi");
@@ -37,16 +25,16 @@ export default function Home() {
       <Banner />
 
       <div className="mx-16">
-        <h1 className="text-4xl my-20 text-center">Our Recipe Categories </h1>
+        <h1 className="text-4xl my-20 text-center italic bg-orange-500 text-white w-[400px] mx-auto rounded pb-1">Our Recipe Categories </h1>
         <div className="grid grid-cols-4 gap-6">
           {categoris?.map((category) => (
             <CategoryCard key={category?.id} category={category} />
           ))}
         </div>
       </div>
-      <div className="mx-16">
-        <h1 className="text-4xl my-20 text-center">Our Newest Recipes </h1>
-        <div className="grid grid-cols-4 gap-6">
+      <div className="mx-16 mb-11">
+        <h1 className="text-4xl my-20 italic bg-orange-500 text-white w-[400px] mx-auto rounded pb-1 text-center">Our Newest Recipes </h1>
+        <div className="grid lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-2 gap-6">
           {recipes
             ?.reverse()
             ?.slice(0, 4)
